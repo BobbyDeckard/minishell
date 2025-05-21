d5ed52d668688726c3a450484bc53c2164fc311d
BobbyDeckard <ivo.meulemans.glasson@gmail.com>
2025-05-21 11:06:51 +0200

    Cleaned up a bit

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: imeulema <imeulema@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 23:47:16 by imeulema          #+#    #+#             */
-/*   Updated: 2025/05/21 11:02:19 by imeulema         ###   ########.fr       */
+/*   Updated: 2025/05/21 11:06:38 by imeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	run_debug(char **envp, char *command);
 
 t_shell_data	g_shell = {NULL, NULL, 0};
 
-/* Real main */
 int main(int ac, char **av, char **envp)
 {
 	t_ast	*ast;
@@ -55,46 +54,3 @@ int main(int ac, char **av, char **envp)
 	}
 	return (0);
 }
-
-/* Debug main */
-/*
-int	main(int ac, char **av, char **envp)
-{
-	t_ast	*ast;
-	char	*command;
-	char	*cwd;
-	int		preset;
-
-	(void) ac;
-	(void) av;
-
-	while (1)
-	{
-		cwd = make_cwd();
-		command = readline(cwd);
-		free(cwd);
-		if (command)
-		{
-			preset = ft_atoi(command);
-			if (preset > 18 || !ft_isdigit(*command))
-			{
-				print_options();
-				free(command);
-			}
-			else
-			{
-				free(command);
-				print_cmd(preset);
-				ast = make_ast(preset);
-				ast->paths = get_paths();
-				ast->envp = envp;
-				exec_ast(ast);
-				cleanup(ast);
-			}
-		}
-		else
-			break ;
-	}
-	return (0);
-}
-*/
